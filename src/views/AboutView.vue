bcec94ac4cabb4fb4af8a4c0438f3fae1e501472
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-09-27 14:44:53 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic iste quo veritatis sit cumque, aspernatur, itaque sed velit amet suscipit voluptate et, distinctio expedita? Excepturi, aliquid fugit. Tempora, dolor totam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sint blanditiis atque impedit ipsam soluta omnis minima debitis distinctio! Aliquid dicta iste eligendi fuga dignissimos dolorem perspiciatis, dolor debitis! Doloremque.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas porro nam a voluptatem numquam ullam. Neque rem voluptatem eum animi temporibus quidem atque quis, eaque adipisci dolorum accusantium, minima fugiat.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam illum earum iure repellendus hic. Aliquid similique nesciunt impedit, nostrum aspernatur magni eos, aut optio exercitationem, et error! Excepturi, tempore corporis!</p>
   </div>
 </template>
